bfdebecbdc8f8fc50bedf06f62cde4d5dbfbd498
Gb <33860784+M3NUKA@users.noreply.github.com>
2022-11-23 15:04:54 +0530

    Update and rename mmm.py to tag_word.py

--- tag_word.py
+++ tag_word.py
@@ -5,7 +5,7 @@ import random
 
 #Set delay to start program
 time.sleep(3)
-
+#this a auto chat program for telegram it will auto reply something, (you writting in 20 line) if someone tell you massage like "mmm".
 def check_newmsg():
     pos2 = pt.locateOnScreen("paper.png", confidence=.7)
 
@@ -17,7 +17,7 @@ def check_newmsg():
                 pt.moveTo(pos2)
                 pt.moveRel(+100, 0)
                 pt.click()
-                pt.typewrite("mmm")
+                pt.typewrite("mmm") #tagged word for "Mmm"
                 pt.press("Enter")
                 
         except():

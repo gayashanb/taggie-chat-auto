5a2967aae674d034dcb0d7296d929cdc3ce16676
Gayashan B <33860784+gayashanb@users.noreply.github.com>
2023-11-07 10:31:59 +0530

    Update tag_word.py

--- tag_word.py
+++ tag_word.py
@@ -2,7 +2,7 @@ import pyautogui as pt
 import time
 import pyperclip
 import random
-
+#This is simple example, I uploaded a little bit expanded version of this program to main
 #Set delay to start program
 time.sleep(3)
 #this a auto chat program for telegram it will auto reply something, (you writting in 20 line)
